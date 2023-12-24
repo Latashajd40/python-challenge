de5e124734aa499018149c1d350422c877620901
Latashajd40 <latashajd@gmail.com>
2023-12-24 01:05:05 -0500

    Pybank completed

--- Pybank/main.py
+++ Pybank/main.py
@@ -4,34 +4,32 @@ import os,csv
 #file path
 budget_file_path = os.path.join("..","Resources","budget_data.csv")
 
+#INITIALIZE VARIABLES
 # The total number of months included in the dataset
-total_of_months = 0
+total_months = 0 
 
 # The net total amount of "Profit/Losses" over the entire period
-total_net_profit_or_loss = 0
+total_profit = 0
 
 # The greatest increase in profits (date and amount) over the entire period
-highest_increase_profits = 0
-highest_increase_profits_date = "1/1/1970"
+largest_increase = 0
+largest_increase_date = "1/1/1970"
 
 #initialize first value for change in profits
-profit_period_one = 0
-profit_period_two = 0
+previous_profit = 0
+current_profit = 0
 
-#create a list of change in profits
+#create list for dates and for change in pnl
 changeInPNL =[]
 changeDates =[]
 
 # The changes in "Profit/Losses" over the entire period, and then the average of those changes
-sum_changes_profit_or_loss = 0.00
-average_change_profit_or_loss = 0.00 
-
-
-# The greatest decrease in profits (date and amount) over the entire period
+change_total = 0.00
+avg_profit = 0.00 
 
 largest_loss_date ="1/1/1970"
 
-#skip reading the header in the data
+
 with open(budget_file_path, encoding='UTF-8') as csvfile:
 
     #create a reader with current file
@@ -44,44 +42,78 @@ with open(budget_file_path, encoding='UTF-8') as csvfile:
     for row in reader_file:
         
         #get total number of months
-        total_of_months+=1
+        total_months+=1
         
         #holds the sum total of losses and gains
-        total_net_profit_or_loss+= round(float(row[1]))
+        total_profit+= round(float(row[1]))
  
            
-        if profit_period_one == 0:
-            profit_period_one = int(row[1])
+        if previous_profit == 0:
+            previous_profit = int(row[1])
         
-        elif profit_period_two == 0:
+        elif current_profit == 0:
             
-            profit_period_two = int(row[1])
+            current_profit = int(row[1])
             
-            sum_changes_profit_or_loss+=profit_period_two-profit_period_one
+            change_total+=current_profit-previous_profit
             
-            changeInPNL.append(profit_period_two-profit_period_one)
+            #add current change to list
+            changeInPNL.append(current_profit-previous_profit)
+            
+            #populate list with corresponding dates
             changeDates.append(row[0])
             
-            profit_period_one = profit_period_two
-            profit_period_two = 0
-            
+            previous_profit = current_profit
+            current_profit = 0
+
+#close reader file            
+csvfile.close()
+
+#greatest loss  
+# find min in list: https://www.tutorialspoint.com/python/list_min.htm        
 largest_loss = min(changeInPNL)
 findIndex = changeInPNL.index(min(changeInPNL))
-largest_loss_date = changeDates[findIndex]          
+largest_loss_date = changeDates[findIndex]  
 
+#greatest increase
+largest_increase = max(changeInPNL)
+findIndex = changeInPNL.index(max(changeInPNL))
+largest_increase_date = changeDates[findIndex]
+
+#average change
+avg_profit = round(sum(changeInPNL)/len(changeInPNL),2)
+
+output =[]
 
 #OUTPUT
-print(f"Financial Analysis")
-print(" ")
-print("----------------------------------")
-print(" ")
-print(f"Total Months: {total_of_months}")
-print(" ")
-print(f"Total: {total_net_profit_or_loss}")
-print(" ")
-print(f"Average Change: {average_change_profit_or_loss}")
-print(" ")
-print(f"Greatest Increase in Profits: {highest_increase_profits_date} ({highest_increase_profits})")
-print(" ")
-print(f"Greatest Decrease in Profits: {largest_loss_date} ({largest_loss})")
+output.append(f"Financial Analysis")
+output.append(" ")
+output.append("----------------------------------")
+output.append(" ")
+output.append(f"Total Months: {total_months}")
+output.append(" ")
+output.append(f"Total: ${total_profit}")
+output.append(" ")
+output.append(f"Average Change: ${avg_profit}")
+output.append(" ")
+output.append(f"Greatest Increase in Profits: {largest_increase_date} (${largest_increase})")
+output.append(" ")
+output.append(f"Greatest Decrease in Profits: {largest_loss_date} (${largest_loss})")
+
+
+#Save results to a text file
+results_path = os.path.join("..","analysis","Results.txt")   
+
+#testing code
+file = open(results_path,'w')   
+
+#write a text file and not a csv file
+#https://www.pythontutorial.net/python-basics/python-write-text-file/
+for x in output:
+    file.write(x)
+    file.write('\n')
+
+ 
+        
+    
 
